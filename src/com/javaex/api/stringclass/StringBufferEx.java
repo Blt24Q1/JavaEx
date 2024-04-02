077af5d6da9949881f305f6adffd77565347d98a
남승균 <skyun.nam@gmail.com>
2024-04-02 16:38:47 +0900

    240402 오후 커밋

--- src/com/javaex/api/stringclass/StringBufferEx.java
+++ src/com/javaex/api/stringclass/StringBufferEx.java
@@ -11,6 +11,25 @@ public class StringBufferEx {
 		//	버퍼 생성
 		StringBuffer sb = new StringBuffer("This");
 		System.out.println(sb);
+		
+		//	문자열 추가 : append
+		sb.append(" is pencil");
+		System.out.println(sb);
+		
+		//	문자열 삽입 : insert
+		sb.insert(7, " my");
+		System.out.println(sb);
+		
+		//	문자열 치환 : replace
+		sb.replace(7, 10, " your");
+		System.out.println(sb);
+		
+		//	메서드 체이닝 : 메서드를 연속적으로 호출, 객체를 변화시키는 방법
+		StringBuffer sb2 = new StringBuffer("this")
+				.append(" is pencil")
+				.insert(7, " my")
+				.replace(7, 10, " your");
+		System.out.println(sb2);
 	}
 
 }
